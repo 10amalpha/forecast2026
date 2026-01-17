bf4e49a5d88ef50462df4be6d7ba3fdcc0cb4017
10amalpha <info@10am.pro>
2026-01-17 08:56:23 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
 }) {
     return (
           <html lang="es">
-                <body style={{ margin: 0, padding: 0 }}>{children}</body>body>
-          </html>html>
+                <body style={{ margin: 0, padding: 0 }}>{children}</body>
+          </html>
         )
-}</html>
+}
